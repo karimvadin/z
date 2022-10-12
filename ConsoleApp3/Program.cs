995d5bbf3cff096b71e3196c5b0ee960e0e1d63d
user1 <user1@526-2>
2022-10-12 12:45:18 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2+2=?");
+            
+            int c = 4;
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (c == b)
+            {
+                while (true)
+                {
+                    Console.WriteLine("z");
+                    Console.WriteLine("v");
+                }
+            }
+            else
+            {
+                Console.WriteLine(":c");
+            }
+               
+
+        }
+    }
+}
